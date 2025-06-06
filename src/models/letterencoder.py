a7afc2ad76edd171ceb34449ae8bc6ad79992062
lancehendricks <hendricks.la@northeastern.edu>
2025-06-06 11:12:56 -0400

    fix: removed device as an argument, now uses device of embedding weights

--- src/models/letterencoder.py
+++ src/models/letterencoder.py
@@ -7,11 +7,10 @@ import torch.nn.functional as F
 # as well as for encoding the state by taking embeddings for each guessed letter (and concat with feedback)
 class LetterEncoder(nn.Module):
     
-    # initializes a LetterEncoder with the given letter embedding dimension and device
-    def __init__(self, letter_embed_dim=16, device=torch.device("cpu")):
+    # initializes a LetterEncoder with the given letter embedding dimension
+    def __init__(self, letter_embed_dim=16):
         super().__init__()
         self.letter_embed_dim = letter_embed_dim
-        self.device = device
 
         # letter embeddings (learnable)
         self.letter_embed = nn.Embedding(26, letter_embed_dim)
@@ -19,7 +18,8 @@ class LetterEncoder(nn.Module):
     # given a single letter, produces its embeddings representation as a torch tensor of dim [self.letter_embed_dim]
     # letter indexing done with generative AI
     def forward(self, letter):
-        letter_idx = ord(letter) - ord('a') # assuming lowercase
-        letter_vec = self.letter_embed(torch.tensor(letter_idx, device=self.device))
+        device = self.letter_embed.weight.device # fetch device of embedding weights for consistency
+        letter_idx = ord(letter) - ord('a') # index of letter - assuming lowercase
+        letter_vec = self.letter_embed(torch.tensor(letter_idx, device=device))
         
         return letter_vec

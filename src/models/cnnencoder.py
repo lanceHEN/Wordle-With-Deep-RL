431cf193f2eab391f7cdf4485b505511e0202432
ameliadsouza <164649807+ameliadsouza@users.noreply.github.com>
2025-06-12 20:18:09 -0400

    Update cnnencoder.py
    added comments to methods

--- src/models/cnnencoder.py
+++ src/models/cnnencoder.py
@@ -1,17 +1,20 @@
 import torch
 import torch.nn as nn
-
-    '''
+'''
+cnnencoder.py:
     Variation of SharedEncoder that has a Convolutional Neural Network front-end.
     expects output of ObservationEncoder [B, 6, 5, per_cell_dim]
     expects meta [B, 2]
     returns [B, output_dim]
-    '''
+'''
 class CNNSharedEncoder(nn.Module):
     '''
     given:
         grid [B, 6, 5, embed_dim]
         meta [B, 2]
+    return:
+    produces a fused representation using:
+        conv2d --> ReLU --> flatten
     '''
     def __init__(self,
                  per_cell_dim: int = 19, # = letter_embed_dim + 3
@@ -21,7 +24,7 @@ class CNNSharedEncoder(nn.Module):
                  output_dim: int = 256):
         super().__init__()
 
-        # ConvNet [C, H = 6, W = 5]
+        # ConvNet [C, H = 6, W = 5] over board
         in_ch = per_cell_dim
         layers = []
         for out_ch in conv_channels:
@@ -39,6 +42,17 @@ class CNNSharedEncoder(nn.Module):
             nn.LayerNorm(board_hidden_dim),
             nn.ReLU(inplace=True),
         )
+        # fuses with meta tensor via Multilayer Perceptron
+        self.fuse = nn.Sequential(
+            nn.Linear(board_hidden_dim + 2, first_hidden_dim),
+            nn.LayerNorm(first_hidden_dim),
+            nn.ReLU(inplace=False),
+            nn.Linear(first_hidden_dim, output_dim),
+            nn.LayerNorm(output_dim),
+            nn.ReLU(inplace=False),
+        )
+
+    # forward pass
     def forward(self, grid, meta):
         # rearranging channel-first for Conv 2d Net [B, C, H=6, W=5]
         x = grid.permute(0, 3, 1, 2).contiguous()

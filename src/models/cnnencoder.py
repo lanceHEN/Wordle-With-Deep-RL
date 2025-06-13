2dec8dae6de8e8969f9fc98bb8e69d1403163854
Lance Hendricks <67243394+lanceHEN@users.noreply.github.com>
2025-06-12 20:44:25 -0400

    changed CNN to treat guesses as channels, changed network to concat with meta tensor right after flattening

--- src/models/cnnencoder.py
+++ src/models/cnnencoder.py
@@ -17,14 +17,12 @@ class CNNSharedEncoder(nn.Module):
         conv2d --> ReLU --> flatten
     '''
     def __init__(self,
-                 per_cell_dim: int = 19, # = letter_embed_dim + 3
-                 conv_channels: tuple = (32, 64, 128),
-                 board_hidden_dim: int = 256,    # after flattening convmap
-                 first_hidden_dim: int = 512,
-                 output_dim: int = 256):
+                per_cell_dim: int = 19, # = letter_embed_dim + 3
+                conv_channels: tuple = (32, 64),
+                first_hidden_dim: int = 512,    # after flattening convmap
+                second_hidden_dim: int = 256):
         super().__init__()
-
-        # ConvNet [C, H = 6, W = 5] over board
+        
         in_ch = per_cell_dim
         layers = []
         for out_ch in conv_channels:
@@ -33,36 +31,31 @@ class CNNSharedEncoder(nn.Module):
                 nn.ReLU(inplace=True),
             ]
             in_ch = out_ch
-        self.conv = nn.Sequential(*layers) # 6×5 resolution
+        self.conv = nn.Sequential(*layers)
+        
+        self.flatten = nn.Flatten()
 
-        # readout to the fixed board embedding
-        self.readout = nn.Sequential(
-            nn.Flatten(), # [B, in_ch * 6 * 5]
-            nn.Linear(in_ch * 6 * 5, board_hidden_dim),
-            nn.LayerNorm(board_hidden_dim),
-            nn.ReLU(inplace=True),
-        )
         # fuses with meta tensor via Multilayer Perceptron
         self.fuse = nn.Sequential(
-            nn.Linear(board_hidden_dim + 2, first_hidden_dim),
+            nn.Linear(6*5*19 + 2, first_hidden_dim),
             nn.LayerNorm(first_hidden_dim),
             nn.ReLU(inplace=False),
-            nn.Linear(first_hidden_dim, output_dim),
-            nn.LayerNorm(output_dim),
+            nn.Linear(first_hidden_dim, second_hidden_dim),
+            nn.LayerNorm(second_hidden_dim),
             nn.ReLU(inplace=False),
         )
 
     # forward pass
     def forward(self, grid, meta):
-        # rearranging channel-first for Conv 2d Net [B, C, H=6, W=5]
-        x = grid.permute(0, 3, 1, 2).contiguous()
+        # grid: [B, 6, 5, D]
 
-        board_emb = self.readout(self.conv(x)) # [B, board_hidden_dim]
-        fused = torch.cat([board_emb, meta], dim = -1)
+        # rearrange so that guesses become channels (like color channels in images)
+        x = grid.permute(0, 3, 2, 1).contiguous()  # [B, D, 5, 6]
+
+        flat = self.flatten(self.conv(x))
+        fused = torch.cat([flat, meta], dim=-1)
         return self.fuse(fused) # [B, output_dim]
-                     
-
-
+                    
                      
 
 

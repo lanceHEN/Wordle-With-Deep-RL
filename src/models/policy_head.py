328fa80c69e74776ef06ff7f3be73876e99eb5ce
lancehendricks <hendricks.la@northeastern.edu>
2025-06-16 18:29:40 -0400

    fixed policy head documentation to correctly describte outputting a query vector

--- src/models/policy_head.py
+++ src/models/policy_head.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 
 
-# given batched latent vectors from the shared encoder, produces predicted word embeddings, which can then be
-# compared with all given word embeddings for all guess words (via dot prod.) to produce logits (masked for valid word indices),
+# given batched latent vectors from the shared encoder, produces a query vector, which can then be
+# multiplied with all given word embeddings for all guess words (via dot prod.) to produce logits (masked for valid word indices),
 # which one can softmax over to get prob. dists.
 # this was chosen over a simpler output head immediately producing logits over every word because embeddings allow for a finer-grained comparison between words,
 # particularly words having similar prefixes or suffixes being treated similarly

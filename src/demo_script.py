e3337c586966b6385c13cd23f7b14c5bc0c5a9e9
lancehendricks <hendricks.la@northeastern.edu>
2025-06-16 18:00:34 -0400

    made separate directory for view

--- src/demo_script.py
+++ src/demo_script.py
@@ -6,7 +6,7 @@ import sys
 from models.wordle_actor_critic import WordleActorCritic
 from models.wordle_model_wrapper import ModelWrapper
 from envs.wordle_game import WordleGame
-from envs.wordle_view import WordleView
+from view.wordle_view import WordleView
 from envs.wordle_env import WordleEnv
 from utils.load_list import load_word_list
 from utils.word_to_onehot import word_to_onehot

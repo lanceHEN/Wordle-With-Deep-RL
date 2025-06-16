4a3f4c19a36a91a25a2256c36ad63b65313941d4
lancehendricks <hendricks.la@northeastern.edu>
2025-06-16 18:15:10 -0400

    added more documentation for policy head

--- src/models/policy_head.py
+++ src/models/policy_head.py
@@ -5,7 +5,8 @@ import torch.nn as nn
 # given batched latent vectors from the shared encoder, produces predicted word embeddings, which can then be
 # compared with all given word embeddings for all guess words (via dot prod.) to produce logits (masked for valid word indices),
 # which one can softmax over to get prob. dists.
-# this was chosen over a simpler output head immediately producing logits over every word because embeddings allow for a finer-grained comparison between words
+# this was chosen over a simpler output head immediately producing logits over every word because embeddings allow for a finer-grained comparison between words,
+# particularly words having similar prefixes or suffixes being treated similarly
 class PolicyHead(nn.Module):
     
     # Initializes a PolicyHead with the given input latent dimension (should be same as SharedEncoder output), and output dimension

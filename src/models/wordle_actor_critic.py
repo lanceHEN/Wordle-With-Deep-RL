81e935db2a39ad301e5a1c23dc2cea11d47281df
lancehendricks <hendricks.la@northeastern.edu>
2025-06-18 16:39:24 -0400

    removed redundant 'per-state' line in actor critic wrapper

--- src/models/wordle_actor_critic.py
+++ src/models/wordle_actor_critic.py
@@ -5,8 +5,8 @@ from models.policy_head import PolicyHead
 from models.value_head import ValueHead
 
 # Wrapper that combines the functionality of all individual components into one, such that given an observation batch, it will produce:
-# 1. Logits over each action (word) for each batch state, i.e. the policy outputs
-# 2. Value predictions, i.e. the value outputs
+# 1. Logits over each action (word) for each batch state, i.e. the policy outputs, for each state in the batch
+# 2. Value predictions, i.e. the value outputs, for each state in the batch
 # For convenience, none of the components need to be given on construction, i.e. any component set to none will be instantiated with
 # default parameters
 class WordleActorCritic(nn.Module):
@@ -27,7 +27,7 @@ class WordleActorCritic(nn.Module):
 
     # Given an observation batch, and word encodings, produces two outputs:
     # 1. Logits over each action (word), i.e. the policy outputs, for each state in the batch
-    # 2. Value predictions, i.e. the value outputs
+    # 2. Value predictions, i.e. the value outputs, for each state in the batch
     def forward(self, obs_batch, word_encodings):
         device = next(self.parameters()).device
         

0855d2fda650ef6497ce8d63be159e7c3df92c9c
lancehendricks <hendricks.la@northeastern.edu>
2025-06-18 16:45:42 -0400

    added more general description to policy head documentation

--- src/models/policy_head.py
+++ src/models/policy_head.py
@@ -2,9 +2,8 @@ import torch
 import torch.nn as nn
 
 
-# given batched latent vectors from the shared encoder, produces a query vector for each batch item, which can then be
-# multiplied with all given word embeddings for all guess words (via dot prod.) to produce logits (masked for valid word indices),
-# which one can softmax over to get prob. dists.
+# Implementation of the policy head that works with batching. Takes in batched latent vectors from the Shared Encoder and 
+# produces a query vector for each batch item, which can be used to obtain a probability distribution
 # this was chosen over a simpler output head immediately producing logits over every word because embeddings allow for a finer-grained comparison between words,
 # particularly words having the same prefixes or suffixes being treated similarly
 class PolicyHead(nn.Module):

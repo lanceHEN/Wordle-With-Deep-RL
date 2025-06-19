335fbd85dc1af56a46712b0629de680f5581c98e
ameliadsouza <164649807+ameliadsouza@users.noreply.github.com>
2025-06-19 17:09:33 -0400

    Update load_list.py

--- src/utils/load_list.py
+++ src/utils/load_list.py
@@ -1,10 +1,9 @@
 # To load a word list from a file
 def load_word_list(file_path: str) -> list:
-    '''Load a word list from a file.
-    
+    '''
+    Load a word list from a file.
     Args:
         file_path (str): Path to the file containing the word list.
-    
     Returns:
         list: List of words loaded from the file.
     '''

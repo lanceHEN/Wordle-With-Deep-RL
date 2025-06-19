d11f844298363cb1a8caa8f7732bcf0b93918e66
ameliadsouza <164649807+ameliadsouza@users.noreply.github.com>
2025-06-18 23:42:10 -0400

    added additional comments to wordle_env.py

--- src/envs/wordle_env.py
+++ src/envs/wordle_env.py
@@ -5,10 +5,10 @@ import math
 class WordleEnv:
     '''
     The WordleEnv class wraps around WordleGame, for use by an RL agent. Its reset method initializes a new game, and
-    its step method, when given a 5 letter word, plays that guess to the internal game and produces the observation, reward, and
+    its step method, when given a 5-letter word, plays that guess to the internal game and produces the observation, reward, and
     whether or not the game ended. The observation is a dict with three keys:
     1. 'feedback', which maps to a list of (guess, results) tuples, where guess is the guessed word and results is a
-        list of lists of "green", "yellow" or "gray" feedback results
+        list of lists of "green", "yellow", or "gray" feedback results
     2. 'turn_number', mapping to the current turn in the game
     3. 'valid_indices', which maps to a list of indices of words in the given word_list that do not contradict existing feedback
     The agent earns a specified win reward if they win, a lose reward if they lose, and an intermediate reward of -1 otherwise
@@ -16,7 +16,7 @@ class WordleEnv:
     
     def __init__(self, word_list, answer_list, win_reward=20, lose_reward=-10):
         '''
-        Initializes a wordle environment, with the given word list, answer list, and win reward
+        Initializes a Wordle environment, with the given word list, answer list, and win reward
         Args:
             word_list: list of valid guesses
             answer_list: list of possible secret words
@@ -33,11 +33,11 @@ class WordleEnv:
 
     def reset(self, word: Optional[str] = None):
         '''
-        resets the environment w/ a brand new game
-        args:
+        Resets the environment with a brand new game
+        Args:
             word: optional secret word for testing
-        returns:
-            the initial observation
+        Returns:
+            The initial observation
         '''
         self.game = WordleGame(self.word_list, self.answer_list, word=word)
         self.candidate_words = list(self.word_list)
@@ -45,44 +45,44 @@ class WordleEnv:
     
     def step(self, guess: str):
         '''
-        Take a step in the environment to make an action by trying a guess. Rewards are +20 for winning, -10 for losing
-        and -1 otherwise
-        args:
+        Takes a step in the environment to make an action by trying a guess. 
+        Rewards are +20 for winning, -10 for losing, and -1 otherwise
+        Args:
             guess: 5-letter word guess
-        returns:
-            tuple of (obs, reward, done), where obs is a dict with three keys:
+        Returns:
+            A tuple of (obs, reward, done), where obs is a dict with three keys:
             1. 'feedback', which maps to a list of (guess, results) tuples, where guess is the guessed word and results is a
-                list of lists of "green", "yellow" or "gray" feedback results
+                list of lists of "green", "yellow", or "gray" feedback results
             2. 'turn_number', mapping to the current turn in the game
             3. 'valid_indices', which maps to a list of indices of words in the given word_list that do not contradict existing feedback 
         '''
-        # plays given guess
+        # Plays given guess
         self.game.play_guess(guess)
 
-        # gets feedback
+        # Gets feedback
         feedback = self.game.get_feedback()
         latest_guess, latest_result = feedback[-1]
         
-        # filter candidate_words by keeping candidates consistent w/ that feedback
+        # Filters candidate_words by keeping candidates consistent with the feedback
         self._filter_cands(latest_guess, latest_result)
         
-        # reward
+        # Reward
         if self.game.is_game_over():
             if self.game.is_won:
-                reward = self.win_reward  # guess is correst
+                reward = self.win_reward  # If guess is correct
             else:
-                reward = self.lose_reward  # game is lost
+                reward = self.lose_reward  # Game is lost
         else:
-            reward = -1 # guess is incorrect
+            reward = -1 # If guess is incorrect
         
-        # return obs and done
+        # Return obs and done
         obs = self._get_obs()
         done = self.game.is_game_over()
         
         return obs, reward, done
     
     
-    # could create a mask instead (?)
+    # Other implementation: create a mask
     def _get_obs(self):
         '''
         get observation of the current env state
@@ -98,7 +98,7 @@ class WordleEnv:
                 'turn_number': 0,
                 'candidate_indices': [] }
         
-        # getting the indices of candidate words in the original word_list
+        # Gets the indices of candidate_words in the original word_list
         candidate_indices = [self.word_to_idx[word] for word in self.candidate_words]
         
         return {
@@ -106,7 +106,7 @@ class WordleEnv:
             'turn_number': self.game.num_guesses,
             'valid_indices': candidate_indices }
     
-    # addt'l to methods, called prev
+    # Additional method
     def _filter_cands(self, guess: str, result: List[str]):
         ''' 
         filters candidate words based on feedback from a guess 
@@ -122,28 +122,28 @@ class WordleEnv:
         '''
         check if a candidate word is consistent w/ result and guess
         '''
-        # simulates the result if candidate word was the secret word
+        # Simulates the result if candidate_word was the secret word
         simulated_result = self._simulate_fb(candidate, guess)
         return simulated_result == result
     
     def _simulate_fb(self, secret: str, guess: str) -> list[str]:
         '''
-        simulates the feedback that would be given for a guess against secret word.
-        this is useful to determine whether the given secret word has feedback matching up with actual feedback,
-        and is useful for filtering candidate words.
+        Simulates the feedback that would be given for a guess against secret word.
+        This is useful to determine whether the given secret word has feedback matching up with actual feedback,
+        and is also useful for filtering candidate words.
         '''
         result = ['gray'] * 5
         secret_chars = list(secret)
         guess_chars = list(guess)
         
-        # mark exact matches in green
+        # Marks the exact matches in green
         for i in range(5):
             if guess_chars[i] == secret_chars[i]:
                 result[i] = 'green'
                 secret_chars[i] = None
                 guess_chars[i] = None
         
-        # mark incorrect place matches in yellow
+        # Marks incorrect place matches in yellow
         for i in range(5):
             if guess_chars[i] is not None:
                 if guess_chars[i] in secret_chars:

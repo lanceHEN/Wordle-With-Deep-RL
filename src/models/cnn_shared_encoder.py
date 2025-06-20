c322b5c15c4809f3d8c05571b53ea58c3be802c9
lancehendricks <hendricks.la@northeastern.edu>
2025-06-20 18:31:40 -0400

    modified CNN shared encoder to reuse FFN in FFN shared encoder

--- src/models/cnn_shared_encoder.py
+++ src/models/cnn_shared_encoder.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+from models.ffn_shared_encoder import FFNSharedEncoder
 '''
 cnn_shared_encoder.py:
     Variation of Shared Encoder that uses a small 2D CNN to summarize the Wordle board. Using a CNN lets us share parameters spatially
-    so the network learns local patterns regardless of their absolute location. Serves as an alternate method to the FFN.
+    so the network learns local patterns regardless of their absolute location. Serves as an alternate method to the standalone FFN.
     expects output of ObservationEncoder [B, 6, 5, per_cell_dim]
     expects meta [B, 2]
     returns [B, shared_output_dim]
@@ -17,7 +18,8 @@ class CNNSharedEncoder(nn.Module):
     Return:
         A fused representation using a set number of convolutions on the grid with specified channels 
         with relu activation at each step, before flattening and concatenating with the meta vector and passing
-        thru an FFN with 2 hidden layers.
+        thru an FFN with 2 hidden layers. Because the FFN is of equivalent design to that in the FFN shared encoder,
+        we actually reuse the FFN forward method here.
     '''
     def __init__(self,
                 per_cell_dim: int = 19, # = letter_embed_dim + 3
@@ -36,19 +38,7 @@ class CNNSharedEncoder(nn.Module):
             in_ch = out_ch
         self.conv = nn.Sequential(*layers)
         
-        self.flatten = nn.Flatten()
-        
-        # fuses with meta tensor via Multilayer Perceptron (to combine conv features with meta)
-        # Note: this is identical to the FFN used in the FFN shared encoder, but we choose not to reuse it
-        # in case we want this FFN backbone to be different in future experimental iterations.
-        self.fuse = nn.Sequential(
-            nn.Linear(in_ch * 6 * 5 + 2, hidden_dim),
-            nn.LayerNorm(hidden_dim),
-            nn.ReLU(inplace=False),
-            nn.Linear(hidden_dim, shared_output_dim),
-            nn.LayerNorm(shared_output_dim),
-            nn.ReLU(inplace=False),
-        )
+        self.ffn = FFNSharedEncoder(in_ch, hidden_dim, shared_output_dim) # Reuse FFN code
 
     # Given batched 3d tensors representing the game grids (word and feedback) of shape [B, max_guesses, word_length, embed_dim]
     # and batched 1d tensors representing the turn and number of candidates remaining for each batch item of shape [B, 2],
@@ -63,13 +53,6 @@ class CNNSharedEncoder(nn.Module):
         # rearrange so that guesses become channels (like color channels in images)
         x = grid.permute(0, 3, 2, 1).contiguous()  # [B, D, 5, 6]
         
-        flat = self.flatten(self.conv(x)) # in_ch * 6 * 5
+        conv = self.conv(x)
 
-        fused = torch.cat([flat, meta], dim = -1) # in_ch * 6 * 5 + 2
-        return self.fuse(fused) # [B, shared_output_dim]
-                    
-                     
-
-
-
-        
+        return self.ffn(conv, meta) # [B, shared_output_dim]

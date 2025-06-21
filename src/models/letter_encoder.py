6e15ce80b91bfd5b0009701b98a2259f847296ba
lancehendricks <hendricks.la@northeastern.edu>
2025-06-21 14:29:02 -0400

    added specification letters must be undercase to letter encoder

--- src/models/letter_encoder.py
+++ src/models/letter_encoder.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Given a letter, produces rich embeddings with the given dimension,
+# Given a lowercase letter, produces rich embeddings with the given dimension,
 # where similar letters will have similar vector representations.
 # This is useful encoding the state by taking embeddings for each guessed letter (and concat with feedback).
 # Note although this is esssentially an nn.Embedding, it is still its own class in case one would like to
@@ -16,7 +16,7 @@ class LetterEncoder(nn.Module):
         # letter embeddings (learnable)
         self.letter_embed = nn.Embedding(26, letter_embed_dim)
 
-    # Given a letter index ('a' being 0, 'z' being 25), produces its embeddings representation as a torch tensor of dim [self.letter_embed_dim].
+    # Given a lowercase letter index ('a' being 0, 'z' being 25), produces its embeddings representation as a torch tensor of dim [self.letter_embed_dim].
     def forward(self, letter_idx):
         device = self.letter_embed.weight.device # fetch device of embedding weights for consistency
         letter_vec = self.letter_embed(letter_idx.to(device))

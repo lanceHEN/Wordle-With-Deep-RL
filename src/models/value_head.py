removed incorrect dimension line in obs shared wrapper

src/models/value_head.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 
-# A critic/value network head that takes in batched latent vectors from SharedEncoder and produces an estimate of the values for their corresponding states as a [B] tensor.
+# A critic/value network head that takes in batched latent vectors from SharedEncoder of shape [B, input_dim]
+# and produces an estimate of the values for their corresponding states as a [B] tensor.
 # This is useful for PPO training.
 # This is a simple MLP with one linear layer - scalar output.
 class ValueHead(nn.Module):
@@ -10,7 +11,7 @@ class ValueHead(nn.Module):
         super().__init__()
         self.value = nn.Linear(input_dim, 1)
      
-    # Given batched vectors from SharedEncoder, produces an estimate of the values for their corresponding states, of shape [B].
+    # Given batched vectors from SharedEncoder of shape [B, input_dim], produces an estimate of the values for their corresponding states, of shape [B].
     def forward(self, h):
         # h: [B, input_dim]
         return self.value(h).squeeze(-1) # [B]